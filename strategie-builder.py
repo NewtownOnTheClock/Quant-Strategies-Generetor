b8f83568b44d85c6160f65b2feef8eea11eae8e1
NewtowN <olivierv@live.ca>
2024-08-26 16:57:08 -0400

    IndicatorPicker imporemented

--- strategie-builder.py
+++ strategie-builder.py
@@ -13,76 +13,78 @@ class Data:
         self.Close = OHLC["Close"]
         self.High = OHLC["High"]
         self.Low = OHLC["Low"]
-        
 
+
+# Momentum Indicators
 class RSI(Data):
     def __init__(self, OHLC) -> None:
         super().__init__(OHLC)
         self.period = self.rnd_period()
-        self.signal = self.rnd_signal()
-        self.upper_bound_signal = self.signal["upper_bound"]
-        self.lower_bound_signal = self.signal["lower_bound"]
+        self.signal = self.rnd_bound()
+        self.upper_bound = self.signal["upper_bound"]
+        self.lower_bound = self.signal["lower_bound"]
 
     def rnd_period(self) -> int:
         return rnd.randint(1, 50)
     
-    def rnd_signal(self) -> int:
+    def rnd_bound(self) -> dict:
         upper_bound = rnd.randint(50, 100)
         lower_bound = rnd.randint(1, 49)
         return {"upper_bound": upper_bound, "lower_bound": lower_bound}
     
     def calculate(self):
-        return ta.RSI(self.Close, self.period)
+        return {f"RSI_{self.period}": ta.RSI(self.Close, self.period)}
 
-class Indicators:
+   
+# Overlap Studies
+class SMA(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.period = self.rnd_period()
+
+    def rnd_period(self) -> int:
+        return rnd.randint(20, 300)
+    
+    def calculate(self):
+        return {f"SMA_{self.period}": ta.SMA(self.Close, self.period)}  
+
+
+class IndicatorsPicker:
     def __init__(self, OHLC) -> None:
         self.OHLC = OHLC
-        self.Close = OHLC["Close"]
-        self.High = OHLC["High"]
-        self.Low = OHLC["Low"]
-        
-        self.indicators = [
-            self.calculate_rsi(), 
-            self.calculate_macd(), 
-            self.calculate_adx(),
-            ]
 
-    def calculate_rsi(self):
-        period = 14
-        return {f"RSI_{period}": ta.RSI(self.Close, period)}
-    
-    def calculate_macd(self):
-        fast_period = 12
-        slow_period = 26 # Slow period must be greater than the fast period
-        signal_period = 9
-        return {f"MACD_{fast_period}_{slow_period}_{signal_period}": ta.MACD(self.Close, fast_period, slow_period, signal_period)[0]}
-    
-    def calculate_adx(self):
-        period = 14
-        return {f"ADX_{period}": ta.ADX(self.High, self.Low, self.Close, period)}
+        self.momentum_indicators = [
+            RSI(self.OHLC)
+        ]
 
-
-class StrategyPicker(Indicators):
-    def __init__(self, OHLC) -> None:
-        super().__init__(OHLC)     
+        self.overlap_studies = [
+            SMA(self.OHLC)
+        ]
 
         self.selected_indicators = self.select_indicators()
     
     def select_indicators(self):
-        number_of_indicator = rnd.randint(1, len(self.indicators))
-        chosen_indicator = rnd.sample(self.indicators, k=number_of_indicator)
-        return chosen_indicator
+        nb_mom_indicators = rnd.randint(1, len(self.momentum_indicators))
+        nb_overlap_studies = rnd.randint(1, len(self.overlap_studies))
+
+        chosen_mom_indicators = rnd.sample(self.momentum_indicators, k=nb_mom_indicators)
+        chosen_overlap_studies = rnd.sample(self.overlap_studies, k=nb_overlap_studies)
+
+        result = {}
+        for mom_indicators, overlap_studies in zip(chosen_mom_indicators, chosen_overlap_studies):
+            result.update(mom_indicators.calculate())
+            result.update(overlap_studies.calculate())
+        return result
     
 
-    def return_df(self):
-        for indicator in self.selected_indicators:
-            for k, v in indicator.items():
-                self.OHLC[k] = v
+    def return_df(self) -> pd.DataFrame:
+        for k, v in self.selected_indicators.items():
+            self.OHLC[k] = v
         return self.OHLC
 
 
-strat = StrategyPicker(df)
-print(strat.return_df())
-print(strat.calculate_macd().keys())
+strat = IndicatorsPicker(df)
+print(strat.return_df().tail())
+
 
 

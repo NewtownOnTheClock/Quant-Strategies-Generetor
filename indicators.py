4a719782992a827fe0744e6ee6096b2939a4a46d
NewtowN <olivierv@live.ca>
2024-09-15 18:24:32 -0400

    UPDATE strategieBuilder + indicators

--- indicators.py
+++ indicators.py
@@ -18,11 +18,11 @@ class RSI(Data):
         return rnd.randint(1, 50)
     
     def rnd_bound(self) -> dict:
-        upper_bound = rnd.randint(50, 100)
+        upper_bound = rnd.randint(50, 99)
         lower_bound = rnd.randint(1, 49)
         return {"upper_bound": upper_bound, "lower_bound": lower_bound}
     
-    def calculate(self):
+    def calculate(self) -> dict:
         return {f"RSI_{self.period}": ta.RSI(self.Close, self.period)}
 
    
@@ -36,13 +36,112 @@ class SMA(Data):
         self.period = self.rnd_period()
 
     def rnd_period(self) -> int:
-        return rnd.randint(20, 300)
+        return rnd.randint(5, 300)
     
-    def calculate(self):
+    def calculate(self) -> dict:
         return {f"SMA_{self.period}": ta.SMA(self.Close, self.period)} 
     
-    def signal(self):
+    def signal(self) -> dict:
         signal = []
         return {f"Signal_SMA_{self.period}": signal} 
 
 
+class EMA(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.indicator_type = {
+            "Relation": "price_related",
+        }
+        self.period = self.rnd_period()
+    
+    def rnd_period(self) -> int:
+        return rnd.randint(5, 300)
+    
+    def calculate(self) -> dict:
+        return {f"EMA_{self.period}": ta.EMA(self.Close, self.period)} 
+    
+
+class HT_TRENDLINE(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.indicator_type = {
+            "Relation": "price_related",
+        }
+
+    def calculate(self) -> dict:
+        return {f"HT_TRENDLINE": ta.HT_TRENDLINE(self.Close)} 
+    
+
+class DEMA(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.indicator_type = {
+            "Relation": "price_related",
+        }
+        self.period = self.rnd_period()
+    
+    def rnd_period(self) -> int:
+        return rnd.randint(5, 300)
+    
+    def calculate(self) -> dict:
+        return {f"DEMA_{self.period}": ta.DEMA(self.Close, self.period)}
+    
+
+class KAMA(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.indicator_type = {
+            "Relation": "price_related",
+        }
+        self.period = self.rnd_period()
+    
+    def rnd_period(self) -> int:
+        return rnd.randint(5, 300)
+    
+    def calculate(self) -> dict:
+        return {f"KAMA_{self.period}": ta.KAMA(self.Close, self.period)}
+    
+
+class MA(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.indicator_type = {
+            "Relation": "price_related",
+        }
+        self.period = self.rnd_period()
+    
+    def rnd_period(self) -> int:
+        return rnd.randint(5, 300)
+    
+    def calculate(self) -> dict:
+        return {f"MA_{self.period}": ta.MA(self.Close, self.period)}
+    
+
+class MIDPOINT(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.indicator_type = {
+            "Relation": "price_related",
+        }
+        self.period = self.rnd_period()
+    
+    def rnd_period(self) -> int:
+        return rnd.randint(5, 300)
+    
+    def calculate(self) -> dict:
+        return {f"MIDPOINT_{self.period}": ta.MIDPOINT(self.Close, self.period)}
+    
+
+class MIDPRICE(Data):
+    def __init__(self, OHLC) -> None:
+        super().__init__(OHLC)
+        self.indicator_type = {
+            "Relation": "price_related",
+        }
+        self.period = self.rnd_period()
+    
+    def rnd_period(self) -> int:
+        return rnd.randint(5, 300)
+    
+    def calculate(self) -> dict:
+        return {f"MIDPRICE_{self.period}": ta.MIDPRICE(self.High, self.Low, self.period)}
--- strategiebuilder.py
+++ strategiebuilder.py
@@ -2,20 +2,31 @@ import pandas as pd
 from fetchdata import Data
 from indicators import *
 import random as rnd
-import yfinance as yf
+import numpy as np
 
 class IndicatorsPicker:
     def __init__(self, OHLC) -> None:
         self.OHLC = OHLC
 
         self.indicators = [
+            # Momentum indicator
             RSI(self.OHLC),
-            SMA(self.OHLC)
+
+            # Overlap studies
+            SMA(self.OHLC),
+            EMA(self.OHLC),
+            HT_TRENDLINE(self.OHLC),
+            DEMA(self.OHLC),
+            KAMA(self.OHLC),
+            MA(self.OHLC),
+            MIDPOINT(self.OHLC),
+            MIDPRICE(self.OHLC),
+
         ]
     
-    def select_indicators(self):
-        nb_long_indicators = rnd.randint(1, len(self.indicators))
-        nb_short_indicators = rnd.randint(1, len(self.indicators))
+    def select_indicators(self) -> pd.DataFrame:
+        nb_long_indicators = rnd.randint(1, 2)
+        nb_short_indicators = rnd.randint(1, 2)
 
         chosen_long_indicators = rnd.sample(self.indicators, k=nb_long_indicators)
         chosen_short_indicator = rnd.sample(self.indicators, k=nb_short_indicators)
@@ -49,15 +60,11 @@ class StategyGenerator(IndicatorsPicker):
     def __init__(self, OHLC) -> None:
         super().__init__(OHLC)
         self.OHLC = OHLC
-        self.indicator_OHLC, self.indicators_info = IndicatorsPicker(self.OHLC).select_indicators()
-        self.possible_indicator_actions = [
-            self.crossover(),
-            self.crossunder(),
-            self.greater_than(),
-            self.lower_than(),
-        ]
+        self.OHLC_and_indicators, self.indicators_info = IndicatorsPicker(OHLC).select_indicators()
+        self.lng_price_related, self.shrt_price_related, self.lng_bnd_realted, self.shrt_bnd_related = self.categorize_indicators()
+
     
-    def categorize_indicators(self):
+    def categorize_indicators(self) -> tuple[list]:
         long_price_related = []
         short_price_related = []
         long_bound_related = []
@@ -74,30 +81,124 @@ class StategyGenerator(IndicatorsPicker):
                 short_price_related.append(s_ind)
             elif self.indicators_info["Short"][s_ind]['Relation'] == "bound_related":
                 short_bound_related.append(s_ind)
+        
+        return long_price_related, short_price_related, long_bound_related, short_bound_related
 
 
-        print(long_price_related, short_price_related)
+    def crossover(self, pair_crossover: bool) -> dict:
+        '''
+        Return signals when the crossover is invoked by the strategy generator
 
-    def crossover(self):
-        pass
+        Parameters:
+        ----------
+        pair_crossover : bool
+            if the crossover is based on one indicator and the asset price, or between two indicators
 
-    def crossunder(self):
-        pass
+        '''
+        previous_close = self.OHLC_and_indicators["Close"].shift(1)
+        results = {}
+        
+        if pair_crossover == 0:
+            for long_ind in self.lng_price_related:
+                results.update({f"L_Signal_{long_ind}": np.where(
+                        (self.OHLC_and_indicators[long_ind] > self.OHLC_and_indicators["Close"]) & 
+                        (self.OHLC_and_indicators[long_ind].shift(1) <= previous_close), 1, 0)})
+                
+        elif pair_crossover == 1:
+            first_ind_data = self.OHLC_and_indicators[self.lng_price_related[0]]
+            second_ind_data = self.OHLC_and_indicators[self.lng_price_related[1]]
+            first_ind_data_lagged = self.OHLC_and_indicators[self.lng_price_related[0]].shift(1)
+            second_ind_data_lagged = self.OHLC_and_indicators[self.lng_price_related[1]].shift(1)
+
+            results.update({f"L_Signal_{self.lng_price_related[0]}_crosso_{self.lng_price_related[1]}": np.where(
+                (first_ind_data > second_ind_data) & 
+                (first_ind_data_lagged <= second_ind_data_lagged), 1, 0)})
+            
+        return results
+
+
+    def crossunder(self, pair_crossunder: bool) -> dict:
+        '''
+        Return signals when the crossunder is invoked by the strategy generator
+
+        Parameters:
+        ----------
+        pair_crossunder : bool
+            if the crossunder is based on one indicator and the asset price, or between two indicators
+
+        '''
+        previous_close = self.OHLC_and_indicators["Close"].shift(1)
+        results = {}
+
+        if pair_crossunder == 0:
+            for short_ind in self.shrt_price_related:
+                results.update({f"S_Signal_{short_ind}" : np.where(
+                        (self.OHLC_and_indicators[short_ind] < self.OHLC_and_indicators["Close"]) & 
+                        (self.OHLC_and_indicators[short_ind].shift(1) >= previous_close), -1, 0)})
+                
+        elif pair_crossunder == 1:
+            first_ind_data = self.OHLC_and_indicators[self.shrt_price_related[0]]
+            second_ind_data = self.OHLC_and_indicators[self.shrt_price_related[1]]
+            first_ind_data_lagged = self.OHLC_and_indicators[self.shrt_price_related[0]].shift(1)
+            second_ind_data_lagged = self.OHLC_and_indicators[self.shrt_price_related[1]].shift(1)
+
+            results.update({f"S_Signal_{self.shrt_price_related[0]}_crossu_{self.shrt_price_related[1]}": np.where(
+                (first_ind_data > second_ind_data) & 
+                (first_ind_data_lagged <= second_ind_data_lagged), -1, 0)})
+            
+        return results
 
     def greater_than(self):
+        results = {}
         pass
 
     def lower_than(self):
         pass
 
-    def long_signal(self):
-        pass
+    def signal(self) -> pd.DataFrame:
+        signal_created = {"Long": [], "Short": []}
+
+        if len(self.lng_price_related) == 1:
+            signal_info = self.crossover(pair_crossover=False)
+            signal_name = next(iter(signal_info))
+            signal_value = next(iter(signal_info.values()))
+            signal_created["Long"].append(signal_info)
+            self.OHLC_and_indicators[signal_name] = signal_value
+
+        elif len(self.lng_price_related) == 2:
+            signal_info = self.crossover(pair_crossover=True)
+            signal_name = next(iter(signal_info))
+            signal_value = next(iter(signal_info.values()))
+            signal_created["Long"].append(signal_info)
+            self.OHLC_and_indicators[signal_name] = signal_value
+        
+        if len(self.shrt_price_related) == 1:
+            signal_info = self.crossunder(pair_crossunder=False)
+            signal_name = next(iter(signal_info))
+            signal_value = next(iter(signal_info.values()))
+            signal_created["Short"].append(signal_info)
+            self.OHLC_and_indicators[signal_name] = signal_value
+            
+        elif len(self.shrt_price_related) == 2:
+            signal_info = self.crossunder(pair_crossunder=True)
+            signal_name = next(iter(signal_info))
+            signal_value = next(iter(signal_info.values()))
+            signal_created["Short"].append(signal_info)
+            self.OHLC_and_indicators[signal_name] = signal_value
+        
+        long_signals = signal_created["Long"]
+        short_signals = signal_created["Short"]
+
+
+
+
+        return self.OHLC_and_indicators
+        
 
 
 df_data = Data.get_OHLC()
 
 strat = StategyGenerator(df_data)
-strat.categorize_indicators()
-
-
+print(strat.indicators_info) 
+print(strat.signal())
 
